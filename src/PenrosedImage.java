ba0d85c86a702430c0da4c182ebbbb92f5ffc6f2
sheldon white <sheldonwhite@comcast.net>
2018-09-14 10:44:40 -0700

    initial commit

---
+++ src/PenrosedImage.java
@@ -0,0 +1,160 @@
+
+import processing.core.*;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PenrosedImage extends PApplet {
+    private boolean drawBorders = true;
+    private float goldenRatio = (1 + sqrt(5)) / 2;
+    private PImage img;
+    private int imgOffsetX, imgOffsetY, penroseSize;
+    public static void main(String args[]) {
+        PApplet.main("PenrosedImage");
+    }
+
+    @Override
+    public void settings() {
+        img = loadImage("image2.jpg");
+        penroseSize = 2 * Math.max(img.width, img.height);
+        size(img.width, img.height);
+        Random r = new Random();
+        imgOffsetX = r.nextInt(400);
+        imgOffsetY = r.nextInt(400);
+        pixelDensity(1);
+        smooth(8);
+    }
+
+    @Override
+    public void draw() {
+        background(0);
+        ArrayList<Tile> tiles = generateWheelTiles(imgOffsetX, imgOffsetY);
+
+        // how many times to divide tiles into sub-tiles
+        int num_subdivisions = 11;
+        for (int deflationNumber = 1; deflationNumber < num_subdivisions; deflationNumber++) {
+            ArrayList<Tile> newTiles = new ArrayList<>();
+            for (Tile tile : tiles) {
+                newTiles.addAll(deflateTile(tile));
+            }
+            tiles = newTiles;
+        }
+        tiles.forEach(t -> {
+            if (t.middle.x > 0 &&
+                    t.middle.x < img.width &&
+                    t.middle.y > 0 &&
+                    t.middle.y < img.height) {
+                t.draw();
+            }
+        });
+        save("penrose.png");
+        print("Done!\n");
+        noLoop();
+    }
+
+
+    private ArrayList<Tile> deflateTile(Tile initialTile) {
+        ArrayList<Tile> newTiles = new ArrayList<>();
+        Point a = initialTile.a;
+        Point b = initialTile.b;
+        Point c = initialTile.c;
+
+        if (initialTile.type == 0){
+            Point p = a.plus((b.minus(a)).divide(goldenRatio));
+
+            newTiles.add(new Tile(0, c, p, b));
+            newTiles.add(new Tile(1, p, c, a));
+        } else {
+            Point q = b.plus((a.minus(b)).divide(goldenRatio));
+            Point r = b.plus((c.minus(b)).divide(goldenRatio));
+
+            newTiles.add(new Tile(1, r, c, a));
+            newTiles.add(new Tile(1, q, r, b));
+            newTiles.add(new Tile(0, r, q, a));
+        }
+
+        return newTiles;
+    }
+
+
+    private ArrayList<Tile> generateWheelTiles(int offsetX, int offsetY) {
+        ArrayList<Tile> tiles = new ArrayList<>();
+
+        Point a = new Point(width/2 + offsetX, height/2 + offsetY);
+        float r = penroseSize/2;
+        float theta;
+
+        for(int i=0; i<10; i++){
+            theta = (2*i - 1) * PI / 10;
+            Point b = new Point(r * cos(theta) + a.x, r * sin(theta) + a.y);
+            theta = (2*i + 1) * PI / 10;
+            Point c = new Point(r * cos(theta) + a.x, r * sin(theta) + a.y);
+
+            // Reverse every other triangle so that it's mirrored, this matters later for decomposition
+            if (i % 2 == 0) {
+                tiles.add(new Tile(0, a, b, c));
+            } else {
+                tiles.add(new Tile(0, a, c, b));
+            }
+        }
+
+        return tiles;
+    }
+
+
+    class Point {
+        float x, y;
+
+        Point(float x0, float y0) {
+            x = x0;
+            y = y0;
+        }
+
+        Point plus(Point p) {
+            return new Point(x + p.x, y + p.y);
+        }
+
+        Point minus(Point p) {
+            return new Point(x - p.x, y - p.y);
+        }
+
+        Point divide(float a) {
+            return new Point(x / a, y / a);
+        }
+
+        Point multiply(float a) {
+            return new Point(x * a, y * a);
+        }
+    }
+
+
+    class Tile {
+        int type;
+        Point a, b, c, middle;
+
+        Tile(int type0, Point a0, Point b0, Point c0) {
+            type = type0;
+            a = a0;
+            b = b0;
+            c = c0;
+            middle = new Point((a.x + b.x + c.x) / 3, (a.y + b.y + c.y) / 3);
+        }
+
+        void draw() {
+            int color = img.get((int)middle.x, (int)middle.y);
+            fill(color);
+
+            if (drawBorders) {
+                stroke(0, 0, 0, 100);
+            } else {
+                noStroke();
+            }
+            triangle(a.x, a.y, b.x, b.y, c.x, c.y);
+
+            boolean debug = false;
+            if (debug) {
+                print(a.x, ",", a.y, " ", b.x, ",", b.y, " ", c.x, ",", c.y, "\n");
+            }
+        }
+    }
+}
---
+++ src/StripedImage.java
@@ -0,0 +1,171 @@
+import processing.core.PApplet;
+import processing.core.PImage;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class StripedImage extends PApplet {
+    private PImage img;
+    private int stripeWidth = 8;
+    private int xcount;
+    private int ycount;
+    private Random r = new Random();
+    private boolean[][] filled;
+    private int[][] imagePoints;
+    int emptySquares;
+
+    public static void main(String args[]) {
+        PApplet.main("StripedImage");
+    }
+
+    @Override
+    public void settings() {
+        img = loadImage("image3.jpg");
+        size(img.width, img.height);
+        pixelDensity(1);
+        smooth(8);
+    }
+
+    @Override
+    public void draw() {
+        xcount = img.width / stripeWidth;
+        ycount = img.height / stripeWidth;
+        imagePoints = new int[xcount][ycount];
+        filled = new boolean[xcount][ycount];
+        emptySquares = xcount * ycount;
+        // Loop through the image, collect array of points
+        for (int y = 0; y < ycount; y++) {
+            for (int x = 0; x < xcount; x++) {
+                int color = img.get(x * stripeWidth, y * stripeWidth);
+                imagePoints[x][y] = color;
+                filled[x][y] = false;
+            }
+        }
+
+        background(0);
+        //noStroke();
+        stroke(0);
+        strokeWeight(1);
+
+//        while (emptySquares > 0) {
+//            int xinit = r.nextInt(xcount), x = xinit;
+//            int yinit = r.nextInt(ycount), y = yinit;
+//            boolean placedOne = false;
+//            do {
+//                if (!filled[x][y]) {
+//                    maximalUniformStripe(x, y);
+//                    placedOne = true;
+//                    break;
+//                }
+//                x++;
+//                if (x == xcount) {
+//                    x = 0;
+//                    y++;
+//                    if (y == ycount) {
+//                        y = 0;
+//                    }
+//                }
+//            } while (x != xinit && y != yinit);
+//        }
+
+        int deltax = r.nextInt(xcount);
+        int deltay = r.nextInt(ycount);
+        for (int x = 0; x < xcount; x++) {
+            int x1 = (x + deltax) % xcount;
+            for (int y = 0; y < ycount; y++) {
+                int y1 = (y + deltay) % ycount;
+                if (!filled[x1][y1]) {
+                    maximalUniformStripe(x1, y1);
+                }
+            }
+        }
+        save("striped.png");
+        print("Done!\n");
+        noLoop();
+    }
+
+    private void maximalUniformStripe(int xInit, int yInit) {
+        int centralColor = imagePoints[xInit][yInit];
+        double maxColorDiff = 30.0;
+        int xMin = xInit, xMax = xInit, yMin = yInit, yMax = yInit;
+
+        // check left
+        for (int x = xInit; x >= 0; x--) {
+            if (filled[x][yInit] || colorDiff(centralColor, x, yInit) > maxColorDiff) {
+                break;
+            }
+            xMin = x;
+        }
+        // check right
+        for (int x = xInit; x < xcount; x++) {
+            if (filled[x][yInit] || colorDiff(centralColor, x, yInit) > maxColorDiff) {
+                break;
+            }
+            xMax = x;
+        }
+        // check up
+        for (int y = yInit; y >= 0; y--) {
+            if (filled[xInit][y] || colorDiff(centralColor, xInit, y) > maxColorDiff) {
+                break;
+            }
+            yMin = y;
+        }
+        // check down
+        for (int y = yInit; y < ycount; y++) {
+            if (filled[xInit][y] || colorDiff(centralColor, xInit, y) > maxColorDiff) {
+                break;
+            }
+            yMax = y;
+        }
+        xMin = min(xMin, xMax);
+        xMax = max(xMin, xMax);
+        yMin = min(yMin, yMax);
+        yMax = max(yMin, yMax);
+        int width = xMax - xMin + 1;
+        int height = yMax - yMin + 1;
+        if (width > height) {
+            paintRect(centralColor, xMin, yMin, width, 1);
+        } else if (height > width) {
+            paintRect(centralColor, xMin, yMin, 1, height);
+        } else {
+            if (r.nextBoolean()) {
+                paintRect(centralColor, xMin, yMin, width, 1);
+            } else {
+                paintRect(centralColor, xMin, yMin, 1, height);
+            }
+        }
+    }
+
+    private void paintRect(int color, int x, int y, int w, int h) {
+        int x0 = x * stripeWidth;
+        int y0 = y * stripeWidth;
+        int width = w * stripeWidth;
+        int height = h * stripeWidth;
+        fill(color);
+        rect(x0, y0, width, height);
+//        textSize(10);
+//        fill(0);
+//        String l1 = x0 + "," + y0;
+//        String l2 = width + "," + height;
+//        text(l1, x0, y0 + 10);
+//        text(l2, x0, y0 + 20);
+        for (int i = x; i < x + w; i++) {
+            for (int j = y; j < y + h; j++) {
+                if (!filled[i][j]) {
+                    filled[i][j] = true;
+                    emptySquares--;
+                }
+            }
+        }
+    }
+
+    private double colorDiff(int baseColor, int x, int y) {
+        int color = img.get(x * stripeWidth, y * stripeWidth);
+        float rDiff = red(baseColor) - red(color);
+        float gDiff = green(baseColor) - green(color);
+        float bDiff = blue(baseColor) - blue(color);
+        double diff = Math.sqrt(rDiff * rDiff + gDiff * gDiff + bDiff * bDiff);
+        return diff;
+    }
+}
